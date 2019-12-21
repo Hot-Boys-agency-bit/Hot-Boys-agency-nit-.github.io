a8a40435e8bb89a12d741d00948e76dcaaa51b46
Hot-Boys-agency-bit <58664713+Hot-Boys-agency-bit@users.noreply.github.com>
2019-12-21 14:55:10 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<form action="send.php" method="POST">
+	
+  <input type="text" name="login">
+  <input type="password" name="pass">
+  <input type="email" name="email">
+  <input type="tel" name="tel">
+  <input type="text" name="name">
+  <input type="text" name="orientation">
+
+  <input type="submit" value="Отправить">
+
+
+
+</form>
